14cce240e8d9311ccfa39a26ace1e133ad309f8e
Rahim <ramzowy@gmail.com>
2024-02-05 10:29:49 -0500

    Lab_04_02

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int springMaintenanceCost = 90;
+        int summerMaintenanceCost = 120;
+        int fallMaintenanceCost = 100;
+        int winterMaintenanceCost = 150;
+        int totalCost = 0;
+
+        totalCost = springMaintenanceCost + summerMaintenanceCost + fallMaintenanceCost + winterMaintenanceCost;
+                System.out.println("The spring cost is " + springMaintenanceCost);
+                System.out.println("The summer cost is " + summerMaintenanceCost);
+                System.out.println("The fall cost is " + fallMaintenanceCost);
+                System.out.println("The winter cost is " + winterMaintenanceCost);
+                System.out.println("The total cost is " +
+                        totalCost);
+
+    }
+}
